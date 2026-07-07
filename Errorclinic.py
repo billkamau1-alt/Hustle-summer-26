190fde62cd3015291654e54d8b2cee902887b9a3
Bill Clinton Kamau <billkamau@Bills-MacBook-Pro.local>
2026-07-07 12:04:59 -0700

    Error clinic done

---
+++ Errorclinic.py
@@ -0,0 +1,72 @@
+
+x = 10 
+y = 0 
+result = "x / y"
+print("Result:", result)
+
+
+
+numbers = [1, 2, 3, 4, 5] 
+for i in range(len(numbers)): 
+ print(numbers[i]) 
+
+
+def calculate_area(radius): 
+ area = 3.14 * radius ** 2 
+ return area 
+radius = 5 
+print(calculate_area(5))
+
+
+def is_even(number): 
+ if number % 2 == 0: 
+    return True 
+ else: 
+    return False 
+print(is_even(4)) 
+print(is_even(7)) 
+
+
+for i in range(5): 
+    print(i) 
+
+
+def greet(name): 
+ return "Hello, " "name" 
+print(greet("Alice")) 
+
+
+numbers = [1, 2, 3, 4, 5] 
+total = 0 
+for number in numbers: 
+    total += number 
+print("Sum of numbers:", "total")
+
+
+def factorial(n): 
+ if "n == 0": 
+    return 1 
+ else: 
+    return "n * factorial(n + 1)" 
+print(factorial(5)) 
+
+
+name = input("Enter your name: ") 
+if "name" == "Alice" or  "Bob": 
+ print("Hello, " + "name") 
+else: 
+ print("Hello, stranger!") 
+
+
+def divide_numbers(x, y): 
+ result = x / y 
+ x=10
+ y=0
+ if y != 0: 
+    return x / y 
+ else: 
+    print("Error: Division by zero is not allowed.") 
+    return None 
+
+
+
